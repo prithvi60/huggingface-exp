7fc1d91082adb13ba441c7105080f3876b42d657
Prithvi <prithvin19@gmail.com>
2025-04-03 17:41:40 +0530

    updates

--- localmodel.py
+++ localmodel.py
@@ -1,12 +1,15 @@
-from smolagents import (CodeAgent, DuckDuckGoSearchTool, OpenAIModel)
+import os
+
+from smolagents import (CodeAgent, DuckDuckGoSearchTool,OpenAIServerModel)
+
 
 # Load Ollama/OpenAI-compatible config
 openai_base_url = os.getenv("OPENAI_BASE_URL", "http://localhost:11434/v1")
-openai_model = os.getenv("OPENAI_MODEL", "mistral")
+openai_model = os.getenv("OPENAI_MODEL", "mistral-7b:latest")
 openai_key = os.getenv("OPENAI_API_KEY", "ollama")
 
-model = OpenAIModel(
-    model=openai_model,
+model = OpenAIServerModel(
+    model_id=openai_model,
     api_base=openai_base_url,
     api_key=openai_key
 )
@@ -21,4 +24,4 @@ agent = CodeAgent(
 )
 
 # Run agent
-agent.run("How many Rubik's Cubes could you fit inside the Notre Dame Cathedral?")
+agent.run("whats 1 + 2 ?")
